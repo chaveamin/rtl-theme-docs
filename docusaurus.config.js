36ed63f980b619b30f831b88f46cae53cc0e56bf
chaveamin <aminchave@gmail.com>
2025-12-08 10:43:14 +0330

    hero

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,8 +2,8 @@ import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "راهنمای محصولات راست چین",
+  tagline: "",
   favicon: "img/favicon.ico",
 
   future: {
